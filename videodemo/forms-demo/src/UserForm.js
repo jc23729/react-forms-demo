db3f2477748744e47d52b16150a42ce82e6209d3
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-10 00:04:42 -0400

    Update UserForm.js

--- videodemo/forms-demo/src/UserForm.js
+++ videodemo/forms-demo/src/UserForm.js
@@ -63,17 +63,23 @@ const UserForm = () => {
 import React, { useState } from "react";
 
 //react will always display the value of whats being show
-const UserForm = () => {
-  const [username, setUsername] = useState("ChickenLady");
-  const handleChange = (e) =>{
-   setUsername(e.target.value)
-  }
+// every time you change anything in the button the state gets updated
+//we have our piece of state that is being used to set the value of this input
+
+// const UserForm = () => {
+//   const [username, setUsername] = useState("ChickenLady");
+//   const handleChange = (e) =>{
+//    setUsername(e.target.value)
+//   }
 
 
-  return (
-    <form>
-      <input type="text" placeholder="username" value={username} onChange={handleChange} />
-  <button>Add me to the list</button>
-    </form>
-  )
+//   return (
+//     <form>
+//       <input type="text" placeholder="username" value={username} onChange={handleChange} />
+//   <button>Add me to the list</button>
+//     </form>
+//   )
+
+
+
 export default UserForm;
